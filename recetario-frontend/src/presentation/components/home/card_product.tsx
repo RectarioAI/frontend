3e3f68158ef8d796f86b105957b2d725c769d6c4
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-09-23 23:45:31 -0500

    update

--- recetario-frontend/src/presentation/components/home/card_product.tsx
+++ recetario-frontend/src/presentation/components/home/card_product.tsx
@@ -2,10 +2,10 @@ import { Image } from "@nextui-org/react";
 import { Product } from "../../../config/helpers/stores-types";
 
 interface CardProductProps {
-  product: Product
+  product: Product;
 }
 
-const CardProduct = ({ product} : CardProductProps  )  => {
+const CardProduct = ({ product }: CardProductProps) => {
   return (
     <div className="w-full h-40 bg-blue-300 rounded-lg flex items-center shadow-lg shadow-blue-200 gap-8 overflow-hidden p-2">
       <Image
@@ -18,10 +18,16 @@ const CardProduct = ({ product} : CardProductProps  )  => {
         <span className="text-medium line-clamp-2 overflow-clip mb-1">
           {product.title}
         </span>
-        <span className="text-small font-bold">Precio: { product.price }</span>
-        <span className="text-small font-bold">Comentarios Positivos: { product.positive_comments}</span>
-        <span className="text-small font-bold">Comentarios Negativos: { product.negative_comments}</span>
-        <span className="text-small font-bold">Comentarios Neutrales: { product.neutral_comments }</span>
+        <span className="text-small font-bold">Precio: {product.price}</span>
+        <span className="text-small font-bold">
+          Comentarios Positivos: {product.positive_comments}
+        </span>
+        <span className="text-small font-bold">
+          Comentarios Negativos: {product.negative_comments}
+        </span>
+        <span className="text-small font-bold">
+          Comentarios Neutrales: {product.neutral_comments}
+        </span>
       </div>
     </div>
   );
